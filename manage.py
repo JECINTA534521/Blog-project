b9262c1a2b074dfd4f811fda34a74d40cfd187c2
jecinta <jecintawanjirug@gmail.com>
2019-10-29 11:06:35 +0300

    this changes development to production

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate,MigrateCommand
 from app import create_app,db
 from app.models import Users,Blogs,Comments
 
-app = create_app('development')
+app = create_app('production')
 
 
 
