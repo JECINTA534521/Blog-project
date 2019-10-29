9edd3c1379f74008bcd53d4333440fad58104c3e
jecinta <jecintawanjirug@gmail.com>
2019-10-29 10:15:53 +0300

    this adds some files in the app folder

---
+++ app/email.py
@@ -0,0 +1,8 @@
+from .import mail
+from flask_mail import Message
+from flask import render_template
+
+def mail_message(subject,template,to,**kwargs):
+    sender_email='jecintawanjirug@gmail.com'
+    email=Message(subject,sender=sender_email,recipients=[to])
+    email.body=render_template(template + '.txt',**kwargs)
