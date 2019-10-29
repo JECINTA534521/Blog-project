6a6e7a8dc9f6ab9698fd40a6e29aec938c89baa4
jecinta <jecintawanjirug@gmail.com>
2019-10-29 10:45:13 +0300

    this adds the sender email address

--- app/email.py
+++ app/email.py
@@ -6,3 +6,7 @@ def mail_message(subject,template,to,**kwargs):
     sender_email='jecintawanjirug@gmail.com'
     email=Message(subject,sender=sender_email,recipients=[to])
     email.body=render_template(template + '.txt',**kwargs)
+
+
+
+
